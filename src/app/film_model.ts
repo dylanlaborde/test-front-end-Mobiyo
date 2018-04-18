4e46956fbed17aab777f6c2e59ffed7aabd01219
dylanlaborde <dylan4047@gmail.com>
2018-04-18 14:33:25 +0200

    modelp de donées pour l'utilisation dans les details

---
+++ src/app/film_model.ts
@@ -0,0 +1,12 @@
+export class film_model {
+	_id : number,
+	isActive : boolean,
+	thumbnail : string,
+	picture : string,
+	year : number,
+	author: string,
+	synopsis : string,
+	registered: Date,
+	tags: string
+	title:string;
+}
